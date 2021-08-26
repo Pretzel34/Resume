8b11753de44fccde85ad19217ba6fc173dddc958
Preston Hrubes <preston.m.hrubes@gmail.com>
2021-08-25 23:41:02 -0600

    added shake to nav bar

--- src/app/root/header/header.component.ts
+++ src/app/root/header/header.component.ts
@@ -1,15 +1,69 @@
-import { Component, OnInit } from '@angular/core';
+import { HostListener, OnInit } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  // ...
+} from '@angular/animations';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
+  animations: [
+    trigger('openClose', [
+      // ...
+      state('open', style({
+        // height: "100px"
+      })),
+      state('closed', style({
+        // opacity: 0.8,
+        // height: '150px'
+      })),
+      transition('open => closed', [
+        animate('1s')
+      ]),
+      transition('closed => open', [
+        animate('0.5s')
+      ]),
+    ]),
+  ],
+  
+  
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  isOpen = true;
+
+  mouseIn = true;
+  mouseOut = false;
+
+  toggle(){
+    this.isOpen = !this.isOpen;
+    console.log('works');
+  }
+
+  
+  constructor() {
+    
+  }
+
+  @HostListener('mouseenter')
+  onMouseEnter() {
+    this.mouseIn = true;
+    console.log('here');
+  }
+
+  @HostListener('mouseenter')
+  onMouseExit() {
+    this.mouseIn = false;
+  }
 
   ngOnInit(): void {
   }
 
+  
 }
